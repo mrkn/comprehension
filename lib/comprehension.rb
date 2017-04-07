0393ddaf2e20015d7aaf47ee8e40e9c49798a80d
Kenta Murata <mrkn@mrkn.jp>
2017-04-07 16:24:30 +0900

    Implement Proc#comprehension and Proc#lazy_comprehension

--- lib/comprehension.rb
+++ lib/comprehension.rb
@@ -1,5 +1,23 @@
 require "comprehension/version"
 
 module Comprehension
-  # Your code goes here...
+  module ProcExtension
+    def lazy_comprehension(first_enum, *rest_enums)
+      enum_for(:comprehension, first_enum, *rest_enums)
+    end
+
+    def comprehension(first_enum, *rest_enums)
+      return lazy_comprehension(first_enum, *rest_enums).map(&:itself) unless block_given?
+
+      if rest_enums.empty?
+        first_enum.each {|x| yield self.(x) }
+      else
+        first_enum.each do |x|
+          ->(*rest_elts) { yield(self.(x, *rest_elts)) }.comprehension(*rest_enums)
+        end
+      end
+    end
+  end
 end
+
+Proc.include Comprehension::ProcExtension
--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -6,6 +6,23 @@ describe Comprehension do
   end
 
   specify do
-    expect(->(i, j){ i * j }.comprehension(1..9, 1..9).to_a).to eq([*1..9].product([*1..9]).map {|i, j| i * j })
+    expect(->(i, j){ i * j }.comprehension(1..9, 1..9)).to eq([*1..9].product([*1..9]).map {|i, j| i * j })
+  end
+
+  specify do
+    expect(->(i, j){ i * j }.comprehension(1..9, 1..9)).to be_an(Array)
+  end
+
+  specify do
+    expect(->(i, j){ i * j }.lazy_comprehension(1..9, 1..9)).to be_an(Enumerator)
+  end
+
+  specify do
+    expect { |b|
+      ->(i){ 2*i }.comprehension(1..3, &b)
+    }.to yield_successive_args(2, 4, 6)
+    expect { |b|
+      ->(i, j){ [i, 10+j] }.comprehension(1..3, 1..3, &b)
+    }.to yield_successive_args([1, 11], [1, 12], [1, 13], [2, 11], [2, 12], [2, 13], [3, 11], [3, 12], [3, 13])
   end
 end
